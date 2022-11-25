4d984b4966f6ea5478f58094159bcbd126ef843a
alstjd974 <alstjd974@nate.com>
2022-11-25 20:26:39 +0900

    2.2~3 Variables
    ● 변수(Variable) : 값을 저장하거나 유지하는 역할
    
    ● const : constant(상수)  바뀌지 않는 값
    
    ● 변수명 유형 : camelCase ex) myName / snake_case ex) my_name
    
    ●let 과 const 차이점
    const는 값이 바뀌지 않지만 // let은 업데이트가 가능하다.
    
    ● const, let 탄생
    기존의 var는 실수로 변수를 업데이트해도 값을 보호받지 못했다.
    그래서 const와 let으로 원하는 것만 업데이트할 수 있게 되었다.
    
    ※
    always : const
    sometimes  : let
    never : var

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "Lee";
+
+const sum = a + b;
+const minus = a - b;
+const mul = a * b;
+const div = a / b;
+console.log(sum + " " + minus + " " + mul + " " + div);
+
+console.log("hello" + " " + myName);
+myName = "min";
+
+console.log("your new name is" + " " + myName);
