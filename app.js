f45b76e739f760e304eb2911d4101d077a8c2e72
alstjd974 <alstjd974@nate.com>
2022-11-25 21:01:15 +0900

    2.4 Booleans
    ● boolean 참(1),거짓(0)으로 이루어진 데이터 타입
    
    ● null : 비어 있다.
    
    ● undegfined : 메모리는 있지만 값이 정의되지 않았다.

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-const a = 5;
-const b = 2;
-let myName = "Lee";
-
-const sum = a + b;
-const minus = a - b;
-const mul = a * b;
-const div = a / b;
-console.log(sum + " " + minus + " " + mul + " " + div);
-
-console.log("hello" + " " + myName);
-myName = "min";
-
-console.log("your new name is" + " " + myName);
+const amIFat = true;
+const amIhandsome = false;
+const nothing = null;
+let something;
