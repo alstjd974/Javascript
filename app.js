d7490ca590e912ca5df1dcd6a07a5a45652b6b2a
alstjd974 <alstjd974@nate.com>
2022-11-26 11:47:54 +0900

    2.5 Array
    ● Array 하나의 변수(variable)안에 데이터 list를 가질 수 있다.
    ex) const nonsense = [1, 2, "hi", false, true, null, undefined];

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const amIFat = true;
-const amIhandsome = false;
-const nothing = null;
-let something;
+const nonsense = [1, 2, "hi", false, true, null, undefined];
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fir", "sat"];
+
+console.log(nonsense);
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the Array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
