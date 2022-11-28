dc427e065796f9c459f05ea27aa3e2ce9c9ec62e
alstjd974 <alstjd974@nate.com>
2022-11-28 17:10:47 +0900

    2.14~16 Conditionals part Tow & Three
    ● 조건문 if(조건)문 : 조건이 참(true)일 시 명령문을 실행하고, 거짓(false)일 시 실행하지 않는다.
    
    ● else if(조건)문 : 다양한 조건을 설정할 수 있게 해준다.
    
    ● else : if/else if문 이 거짓(false) 명령문을 실행한다.
    
    ※ 조건이 중첩되어 조건문 누락 될 수 있으므로 조건문 순서를 잘 설정해야 한다.
    
    ● 논리 연산자(AND(&&) / OR(||))
    ○ AND : 조건이 둘 다 참이 여야 참이 된다.
    ○ OR : 조건 둘 중 하나라도 참이면 참이 된다.
    
    ● 다양한 조합이 가능 하다.
    ex (a && b) || (c && d)

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const age1 = prompt("How old are you");
-const age2 = parseInt(prompt("How old are you?"));
+const age = parseInt(prompt("How old are you?"));
 
-console.log(typeof age1, age1);
-console.log(typeof age2, age2);
+//console.log(isNaN(age));
+
+if (isNaN(age || age < 0)) {
+  console.log("Please write a real positive number.");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+  console.log("You shold exercise.");
+} else if (age === 100) {
+  console.log("wow you are wise.");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
+}
