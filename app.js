78951d5bb17735359978b4e9bc5a04eb86deb3fa
alstjd974 <alstjd974@nate.com>
2022-11-29 20:01:20 +0900

    3.2 Searching For Elements
    ● document는 HTML이 해당 JS를 load하기 때문에 존재한다.
    → <script scr = "해당 JS 파일"></script>
    
    ● document 객체를 통해 여러 함수 getElementsBy(Id, ClassName, TegName)를 사용하여 HTML요소들을 JS로 가져올 수 있다.
    → getElementsBy(id,ClassName,TegName)는 요소를 배열(Array)로 리턴
    
    ● querySelector는 element를 CSS 방식으로 검색할 수 있다.
    → 특정 class 내부에 특정 요소를 가지고 올 수 있다는 것을 의미한다.
    
    ○ querySelector는 첫 번째 요소를 리턴 (:first - child)
    
    ○ querySelectorAll selector안의 조건에 부합하는 모든 element를 리턴
    
    ● querySelector ID/Class 전달 시
    ○ id : document.querySelector("#id명");
    ○ class : document.querySelector(".class명");
    → querySelector에서는 CSS selector 자체를 전달하기 때문이다.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const titleId = document.getElementById("title");
-//title.innerText = "Got you!";
+const title1 = document.getElementsByClassName("title");
+const title2 = document.querySelector(".hello h1");
+const title3 = document.querySelectorAll(".hello h1");
 
-console.log(title.className);
+console.log(title1);
+console.log(title2);
+console.log(title3);
+
+title2.innerText = "Hello";
