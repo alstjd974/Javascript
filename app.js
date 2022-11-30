8ddca5492a537194c7ee80084ce9b7bceb0d072d
alstjd974 <alstjd974@nate.com>
2022-11-30 20:24:50 +0900

    3.8 CSS in JS(3)
    ● CSS에 정의된 class name들로 HTML element에서 숨기고 표시하는 방법
    
    ※문제점 : className은 모든 걸 교체해버린다. (이전의 class들 상관 X)
    ● classList : class들의 목록으로 작업할 수 있게 허용해 준다.
    contains, add, remove 함수, 등 을 사용하여 포함 여부, 추가, 삭제 등이 가능
    
    ● toggle 함수 : class name이 존재하는지 확인
    → class name이 존재 시 toggle은 class name을 제거 / 존재하지 않는다면 추가

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  //const clickedClass = "clicked";
+  h1.classList.toggle("clicked");
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
 }
 
 h1.addEventListener("click", handleTitleClick);
