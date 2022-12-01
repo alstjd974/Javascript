f792fcc9cde9b797fb7d7b511f93a927142864e2
alstjd974 <alstjd974@nate.com>
2022-12-01 18:59:42 +0900

    4.2~3 Event
    ● HTML에 JS를 import 하고 있기 때문에 JS로 HTML 요소(elements)들을 가지고 올 수 있다
    → document가 HTML이 JS 파일을 load 하기 때문에 존재
    → 그다음에 브라우저가 document에 우리가 접근할 수 있게 해준다.
    
    ● JS object 내부에 있는 속성(property)들의 값들을 변경할 수 있다.
    → console.dir(element)를 통해 object로 표시한 element 내부를 확인할 수 있다.
    → JS에서 대부분 event를 listen 하는 작업을 한다. (style → CSS 파일)
    
    ● event : 어떤 행위를 하는 것 (click, enter, 등)
    
    ● eventListener : JS에 어떤 event를 listen할 건지 알려주고 해당 이벤트가 발생 시 JS가 해당 함수를 실행시켜준다.
    → (HTML에서 가져온 요소).addEventListener("event 종류" , event 발생 시 실행할 함수명);
    
    ※ 모든EventListener 함수의 첫 번째 인자(argument)는 항상 발생한 이벤트의 정보를 담고 있다.
    → EventListener에 어떤 함수를 추가하던 JS에서는 첫 번째 인자로 발생된 event에 대한 정보(event object)를 제공한다.
    
    ● 브라우저는 이벤트 발생 시 새로고침을 하고 form을 자동으로 submit 한다.(기본동작)
    →. preventDefault(); 함수로 기본동작을 막는다.

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// const loginForm = document.querySelector("#logIn-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
+const logInForm = document.querySelector("#login-form");
+const logInInput = document.querySelector("#login-form input");
 
-const loginInput = document.querySelector("#logIn-form input");
-const loginButton = document.querySelector("#logIn-form button");
+const link = document.querySelector("a");
 
-function btnClick() {
-  const username = loginInput.value;
-  // if (username === "") {
-  //   alert("please write your name.");
-  // } else if (username.length > 15) {
-  //   alert("your name is too long.");
-  // } else {
-  //   console.log("hello", " ", username);
-  // }
-  console.log("hello", " ", username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = logInInput.value;
+  console.log(username);
+  console.log(event);
 }
 
-loginButton.addEventListener("click", btnClick);
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+logInForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
