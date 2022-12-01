6bcab2d6cda43f094dec8d34da67493b75ed6714
alstjd974 <alstjd974@nate.com>
2022-12-02 03:50:24 +0900

    4.4~6 Get+Save+Load/(ing) Username
    ● Getting/Saving Username
    1. HTML에서 id,class 등 을 이용하여 element를 가져온다.
    2. EventListener를 통해 해당 element에 지정한 이벤트 발생 시 지정한 함수가 실행 되게 한다.
    3. CSS문서에 사라지게 하는 기능 hidden 클래스를 만들어준다.
    
    1. input에 이름을 입력하고 버튼을 클릭 시 submit이 실행된다.
    → submit은 form태그 사용 시 브라우저가 제공하는 기능이다.
    
    2. form 요소를 담은 변수에 eventListener를 추가하여
    인자로 submit("특정 이벤트") 와 event 발생 시 실행될 함수(logInBtnSubmit())를 지정해 준다.
    
    3. logInBtnSubmit() 함수의 첫 번째 인자는 해당 이벤트 정보를 담고 있다.
    → 인자에 preventDefault() 함수를 사용하여 submit의 기본동작을 막는다
    
    4. 유저이름을 입력 시 이 값은 logInInput 요소의 value에 저장되는데 이 값을
    새로운 username이라는 변수를 선언하여 넣어준다.
    → 여러 번 사용 되기 때문에 변수에 넣어주면 오타시 JS에서 오류의 위치를 알려준다.
    
    5. logInForm요소에 히든 클래스를 classList.add()로 hidden 클래스를 추가 해준다.
     → logInForm이 화면에서 사라진게 된다.
    
    6. greeting(h1)요소에 inner.Text(안의 내용)에 username(Input 입력 값)을 넣어준다.
    
    7. greeting 요소에는 히든 클래스를 classList.remove()로 제거하여 화면에 출력되게 해준다.
    __________________________________________________________________________________________________________
    ● Loading Username
    
    ● 데이터를 저장하고 기억하기 위해 다양한 기능을 브라우저에서 제공한다
    
    ● localStorage.setItem("키 명", 키 값); : localsStorage에 키 명과 키 값을 설정한다.
    ● localStorage.getItem("키 명"); : localsStorage에 데이터를 조회해 가져온다.
    ● localStorage.remove("키 명"); : localsStorage에 데이터 삭제
    
    0. logInbtnSumit 함수에 username(Input 입력 값)을 loacalStorage에 저장한다.
    → localStorage.setItem(USERNAME_KEY("키 명"), username);
    
    1. localStorage에 USERNAME_KEY 유무를 판단한다.
    → localStorage.getItem(USERNAME_KEY)로 조회하고 키 값을 가져온다.
    → saveUsername이라는 변수를 선언하여 가져온 키 값을 안에 넣어준다.
    
    2. if(saveUsername === null) 만약 이 값이 null(비어있음)이라면
    → logInBtnSubmit()에 hidden class를 classList.remove()로 제거한다.
    →  logInForm.addEventListener("submit", logInBtnSubmit);
    절차에 따라 submit 이벤트 발생 시 지정된 함수가 실행된다.
    → 0번 과정을 거치고 입력 값을 출력
    
    3. else{localStorage 유저 값이 존재할 때}
    → 값 출력

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const logInForm = document.querySelector("#login-form");
-const logInInput = document.querySelector("#login-form input");
+const logInForm = document.querySelector("#login-form"); // 1.
+const logInInput = document.querySelector("#login-form Input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
-function btnLogInClick(event) {
+function logInBtnSubmit(event) {
   event.preventDefault();
-  logInForm.classList.add(HIDDEN_CLASSNAME);
   const username = logInInput.value;
+  logInForm.classList.add(HIDDEN_CLASS);
 
-  greeting.innerText = `Hello ${username}`; // === "Hello" + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+
+  paintGreeting(username);
 }
 
-logInForm.addEventListener("submit", btnLogInClick);
+function paintGreeting(username) {
+  greeting.classList.remove(HIDDEN_CLASS);
+  greeting.innerText = `Hello ${username}`;
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if (saveUsername === null) {
+  logInForm.classList.remove(HIDDEN_CLASS);
+  logInForm.addEventListener("submit", logInBtnSubmit); // 2.
+} else {
+  paintGreeting(saveUsername);
+}
