7246c79067729acd307f84f51cd24e858b310905
alstjd974 <alstjd974@nate.com>
2022-12-06 22:09:26 +0900

    5.2~3 padStart
    ● padStart() : 현재 문자열의 시작을 다른 문자열로 채워, 주어진 길이를 만족하는 새로운 문자열을 반환합니다.
    → 채워넣기는 대상 문자열의 시작(좌측)부터 적용됩니다.
    
    ● 구문
    ○ str.padStart(targetLength, "padString");
    
    ● 매개변수
    ○ targetLength :  목표 문자열 길이. 현재 문자열의 길이보다 작다면 채워넣지 않고 그대로 반환.
    
    ○ "padString" : 현재 문자열에 채워넣을 다른 문자열.
    → 문자열이 너무 길어 목표 문자열 길이를 초과한다면 좌측 일부를 잘라서 넣음. 기본값은 " ".
    
    참고 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
    
    ● Date.prototype.toLocaleTimeString() :
    이 toLocaleTimeString()메서드는 날짜의 시간 부분을 언어별로 표현한 문자열을 반환합니다.
    Intl.DateTimeFormatAPI를 지원 하는 구현 에서 이 메서드는 단순히 를 호출합니다 Intl.DateTimeFormat.
    ex) console.log(date.toLocaleTimeString("ko-KR")); // "오후 12:00:00"
    
    참고 : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString

--- clock.js
+++ clock.js
@@ -1,8 +1,20 @@
 const clock = document.querySelector("h2#clock");
-
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  let AMPM = "";
+  let hour = date.getHours();
+  const min = String(date.getMinutes()).padStart(2, "0");
+  const sec = String(date.getSeconds()).padStart(2, "0");
+  if (hour > 12) {
+    AMPM = "PM";
+    hour -= 12;
+  } else {
+    AMPM = "AM";
+  }
+  hour = String(hour).padStart(2, "0");
+
+  clock.innerText = `${AMPM} ${hour}:${min}:${sec}`;
+  // clock.innerText = date.toLocaleTimeString();
 }
 
 getClock();
